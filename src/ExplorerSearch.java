fee8026af31c2a3632b7b07ffcf5553b9762257a
unknown <174657090+Butenkite@users.noreply.github.com>
2025-06-02 12:50:54 -0700

    initial reachableArea setup

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -33,18 +33,36 @@ public class ExplorerSearch {
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
 
-    // explorer helper
-        // through each column
-            // through each row
-                // if zero
-                   // set location
-        // else no explorer
-            // return 0 - there is no explorer, so there is nothing to explore.
+        // size of island
+        int rows = island.length;
+        if(rows == 0) return 0;
+        int cols = island[0].length;
+        // explorer coordinates
+        int x = -1;
+        int y = -1;
+        // visited 
+        boolean[][] visited = new boolean[rows][cols];
 
-        // have a visited 
+        // find explorer
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (island[i][j] == 0) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        // didnt find it
+        if (x == -1 || y == -1) return 0;
+
         // recurse through helper
         // return amount of tiles in visited
         return -1;
+    }
+
+    public static void reachableAreaHelper(int[][] island, int row, int col, boolean[][] visited){
+
     }
 
     // helper params: currentCoordinates(first time explorer coordinates), island, visited
@@ -53,5 +71,4 @@ public class ExplorerSearch {
         // check in each direction from the coordinates
             // if in range of island AND traversable
                // recurse at coordinate
-
 }
