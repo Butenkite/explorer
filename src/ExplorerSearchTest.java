b182de2bed09c8105b37cc932b1fa23601bab965
unknown <174657090+Butenkite@users.noreply.github.com>
2025-06-02 11:05:48 -0700

    Starting tests

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -14,6 +14,43 @@ public class ExplorerSearchTest {
         int actual = ExplorerSearch.reachableArea(island);
         assertEquals(14, actual);
     }
+    @Test
+    public void testReachableArea_onlyMountains() {
+        int[][] island = {
+            {3,3,3,3,3,3},
+            {3,3,3,3,3,3},
+            {3,3,3,3,3,3},
+            {3,3,3,3,0,3},
+            {3,3,3,3,3,3},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+    @Test
+    public void testReachableArea_onlyPlains() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,0,1},
+            {1,1,1,1,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(30, actual);
+    }
+
+        @Test
+    public void testReachableArea_snake() {
+        int[][] island = {
+            {0,1,1,1,1,1},
+            {3,3,3,3,3,1},
+            {1,1,1,1,1,1},
+            {1,3,3,3,3,3},
+            {1,1,1,1,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(20, actual);
+    }
 
     // Add more tests here!
     // Come up with varied cases
